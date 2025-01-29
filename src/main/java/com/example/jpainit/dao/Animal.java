7385d446f9c9d5a14517ca5d3609d760b6070d6c
Yanis ADEKALOM <yanis971.60@gmail.com>
2025-01-29 16:56:50 +0100

    Feat: AnimalRepository.search

--- src/main/java/com/example/jpainit/dao/Animal.java
+++ src/main/java/com/example/jpainit/dao/Animal.java
@@ -3,11 +3,13 @@ package com.example.jpainit.dao;
 import com.example.jpainit.entities.heritage.mapped_superclass.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.NamedQuery;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Entity
+@NamedQuery(name= "Animal.namedFindAll", query = "FROM Animal")
 public class Animal extends BaseEntity {
     private String nom;
     private LocalDate birthday;
--- src/main/java/com/example/jpainit/dao/jpa/AnimalRepository.java
+++ src/main/java/com/example/jpainit/dao/jpa/AnimalRepository.java
@@ -2,6 +2,7 @@ package com.example.jpainit.dao.jpa;
 
 import com.example.jpainit.dao.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,6 +15,11 @@ public interface AnimalRepository extends JpaRepository<Animal, Long> {
     Animal findByBirthdayBetween(LocalDate birthdayAfter, LocalDate birthdayBefore);
     Animal findByNomLikeAndBirthdayAfter(String nom, LocalDate birthdayAfter);
 
-    /* SELECT * FROM Animal nom LIKE %:nom% OR birthday = :birthday */
+
+    @Query("""
+FROM Animal 
+WHERE (:nom IS NULL OR nom LIKE %:nom%) 
+AND (:birthday IS NULL OR birthday = :birthday)
+""")
     List<Animal> search(String nom, LocalDate birthday);
 }
